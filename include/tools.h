75124559681d5edca2cccb13f3cc2ae80b426ee3
bytemare <d@bytema.re>
2019-01-06 01:28:15 +0100

    Added a secure random string generator function, in a dedicated src/tools.c file, for now

--- include/tools.h
+++ include/tools.h
@@ -7,4 +7,8 @@
 #ifndef SECURE_BROKER_TOOLS_H
 #define SECURE_BROKER_TOOLS_H
 
+#include <stdint.h>
+
+static void secure_random_string(char *rand, uint32_t size);
+
 #endif //SECURE_BROKER_TOOLS_H
--- src/tools.c
+++ src/tools.c
@@ -4,4 +4,29 @@
  * Copyright (C) 2015-2019 Bytemare <d@bytema.re>. All Rights Reserved.
  */
 
-#include "tools.h";
+#include <string.h>
+#include <stdint.h>
+
+/* BSD */
+#include <bsd/stdlib.h>
+
+/**
+ * Fills the buffer pointed to by *rand with random alphanumerical values.
+ * The function never fails and will always return, and is based on a BSD random generator.
+ * @param rand
+ * @param size
+ * @return
+ */
+static void secure_random_string(char *rand, uint32_t size){
+
+    static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    uint32_t n, charset_length = (uint32_t) strlen(charset);
+
+    if (size){
+        --size;
+        for (n = 0; n < size; n++){
+            rand[n] = charset[arc4random_uniform(charset_length)];
+        }
+        rand[size] = '\0';
+    }
+}
