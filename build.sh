828dfbf1533a690e9e2cf96967afb19e79aeb94a
bytemare <{ID}+{username}@users.noreply.github.com>
2019-05-16 18:22:55 +0200

    corrected some formatting to fit shellcheck

--- build.sh
+++ build.sh
@@ -1,36 +1,34 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-# Copyright (C) 2017-2018 Bytemare <d@bytema.re>. All Rights Reserved.
-
-#! /bin/bash
+#!/usr/bin/env sh
 
 set -e
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2017-2018 Bytemare <d@bytema.re>. All Rights Reserved.
 
 #
 # Parameter gathering and building out-of-source with CMAKE
 #
 
 
-
 # Check if file exists
 check_file()
 {
-    [ -f $1 ] || ( echo "\n[Error] $1 does not exist !" && exit 1)
+    [ -f "$1" ] || ( printf "\n[Error] %s does not exist !" "$1" && exit 1)
 }
 
-echo -n "Checking for build files ..."
+printf "Checking for build files ..."
 
-PARAMETERS="parameters.conf" && check_file ${PARAMETERS}
-CMAKELISTS="CMakeLists.txt" && check_file ${CMAKELISTS}
+PARAMETERS="./parameters.conf" && check_file ${PARAMETERS}
+CMAKELISTS="./CMakeLists.txt" && check_file ${CMAKELISTS}
 
-echo " done."
-
-
+printf " done.\n"
 
 #
 # Source parameter file
 #
 
-source ${PARAMETERS}
+# shellcheck source=parameters.conf
+. ${PARAMETERS}
 
 
 # Targets to be created and deleted
@@ -42,15 +40,15 @@ $s
 
 
 # Build cleaner script
-echo $SHELL_H > ${CLEANER}
-echo $s >> $CLEANER
-chmod 700 $CLEANER
+echo "$SHELL_H" > "$CLEANER"
+echo "$s" >> "$CLEANER"
+chmod 700 "$CLEANER"
 
 
 # Build Release
-mkdir -p $RELEASE
-cd $RELEASE
-cmake -DCMAKE_BUILD_TYPE=$RELEASE ..
+mkdir -p "$RELEASE"
+cd "$RELEASE"
+cmake -DCMAKE_BUILD_TYPE="$RELEASE" ..
 make
 s="./$RELEASE/$LINK "
 
@@ -58,8 +56,8 @@ s="./$RELEASE/$LINK "
 cd ..
 
 # Build Debug
-mkdir -p $DEBUG
-cd $DEBUG
+mkdir -p "$DEBUG"
+cd "$DEBUG"
 #cmake -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 #make
 #s="./$DEBUG/$LINK "
@@ -75,19 +73,19 @@ cd ..
 
 
 # Build launching script
-s="./$DEBUG/$LINK "
-s=$s"socket_path=$SOCKET_PATH "
-s=$s"mq_name=$MQ_NAME "
-s=$s"log_file=$LOG_FILE "
-s=$s"domain=$DOMAIN "
-s=$s"protocol=$PROTOCOL "
-s=$s"max_connections=$MAX_CONNECTIONS "
-s=$s"socket_permissions=$SOCKET_PERMISSIONS "
-s=$s"authorised_peer_username=$AUTHORISED_PEER_NAME "
-s=$s"verbosity=$VERBOSITY "
+s="./$DEBUG/$LINK"
+s=$s" socket_path=$SOCKET_PATH"
+s=$s" mq_name=$MQ_NAME"
+s=$s" log_file=$LOG_FILE"
+s=$s" domain=$DOMAIN"
+s=$s" protocol=$PROTOCOL"
+s=$s" max_connections=$MAX_CONNECTIONS"
+s=$s" socket_permissions=$SOCKET_PERMISSIONS"
+s=$s" authorised_peer_username=$AUTHORISED_PEER_NAME"
+s=$s" verbosity=$VERBOSITY"
 
 
 
-echo $SHELL_H > $RUNNER
-echo $s >> $RUNNER
-chmod 700 $RUNNER
+echo "$SHELL_H" > "$RUNNER"
+echo "$s" >> "$RUNNER"
+chmod 700 "$RUNNER"
