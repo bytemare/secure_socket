2639a664959b7540525e25417647645dcfb467da
bytemare <{ID}+{username}@users.noreply.github.com>
2019-06-18 16:13:37 +0200

    clean up

--- src/handler.c
+++ src/handler.c
@@ -15,7 +15,6 @@ void handler(thread_context *ctx){
     int read_size;
     char client_message[2000];
 
-    //read_size = (int) recv(ctx->socket->socket_fd , client_message , 2000 , 0);
     read_size = ipc_recv(ctx->socket, client_message, 2000, ctx);
 
     if (read_size == -1){
@@ -29,16 +28,8 @@ void handler(thread_context *ctx){
         fflush(stdout);
     }
 
-    printf("Server received '%s'\n", client_message);
-    printf("Server sending '%s'\n", client_message);
-
-    /*if(write(ctx->socket->socket_fd , client_message , strlen(client_message)) == -1){
-        perror("SERVER : write returned with error\n");
-        return;
-    }*/
+    printf("\033[0;32mServer received '%s'\033[0m\n", client_message);
+    printf("\033[0;32mServer sending '%s'\033[0m\n", client_message);
 
     ipc_send(ctx->socket, read_size, client_message, ctx);
-
-    return;
-
 }
